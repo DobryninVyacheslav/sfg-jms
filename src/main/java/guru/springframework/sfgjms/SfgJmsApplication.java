9196357ddb1bb9ebfb8f11df0a548890a95d992b
DobryninVyacheslav <w.v.dobr@gmail.com>
2021-02-17 07:05:53 +0300

    remove embedded jms server

--- src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
+++ src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
@@ -1,9 +1,6 @@
 package guru.springframework.sfgjms;
 
 import lombok.SneakyThrows;
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,16 +9,6 @@ public class SfgJmsApplication {
 
     @SneakyThrows
     public static void main(String[] args) {
-
-        ActiveMQServer activeMQServer = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
-                .setPersistenceEnabled(false)
-                .setJournalDirectory("target/data/journal")
-                .setSecurityEnabled(false)
-                .addAcceptorConfiguration("invm", "vm://0")
-        );
-
-        activeMQServer.start();
-
         SpringApplication.run(SfgJmsApplication.class, args);
     }
 
